191dc691aca9833b8ee584f4db7c741a8f5bb925
shiryjona <shiryj@cs.colman.ac.il>
2026-01-18 23:22:06 +0200

    Delete defult lines from creation.

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -16,10 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
         tvStatus = findViewById(R.id.tvStatus)
         btnPlayAgain = findViewById(R.id.btnPlayAgain)
